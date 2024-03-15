10a7a4e79258dcfcf54332411f439fce340badd9
Muro <105938976+Muro17@users.noreply.github.com>
2024-03-15 11:36:14 -0300

    dsadasdas

---
+++ lanchinho.js
@@ -0,0 +1,11 @@
+let TamanhoDaPeça = 2;
+
+let chupe = prompt('Qual tamanho da minha peça?');
+
+if(TamanhoDaPeça == chupe){
+    alert('broxa kkkkkkkk');
+    console.log('broxa kkkkkkkk');
+} else{
+    alert('errou esquisito');
+    console.log('errou estranho');
+}
