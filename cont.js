f52755c4884e6e5d4c7559256d629dae886e9ced
Muro <105938976+Muro17@users.noreply.github.com>
2024-04-05 12:20:07 -0300

    dsadas

--- cont.js
+++ cont.js
@@ -4,7 +4,11 @@ const botinhos = document.querySelectorAll('.objetivos');
 for (let x = 0; x<botinhos.length; x++){
     console.log(x)
     botinhos[x].onclick = function() { 
+for(let y = 0; y< botinhos.length; y++){ 
+    botinhos[y].classList.remove('ativo');
+}
         botinhos[x].classList.add('ativo');
 
     }
+    botinhos[x].classList.remove('ativo');
 }
