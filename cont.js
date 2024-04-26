a3147f9f1659d092a1d48315f9372969854732f4
Muro <105938976+Muro17@users.noreply.github.com>
2024-04-26 08:47:45 -0300

    dadsa

--- cont.js
+++ cont.js
@@ -21,5 +21,8 @@ let tempoObjetivo1 = new Date ('Fri Dec 31 3999 23:59:59');
 let tempoFinal = tempoObjetivo1 - tempoAtual;
 
 let segundos = Math.floor(tempoFinal/1000);
+let minutos = Math.floor(segundos/60);
+let horas = Math.floor(minutos/60);
+let dias = Math.floor(horas/24);
 
-contadores[0].textContent = segundos;
+contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos. ";
