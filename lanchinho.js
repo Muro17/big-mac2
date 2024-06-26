cc61fc44f9d1c3c433fda7ef0d563cdd84762055
Muro <105938976+Muro17@users.noreply.github.com>
2024-06-25 21:36:17 -0300

    sadsa

--- lanchinho.js
+++ lanchinho.js
@@ -1,17 +1,17 @@
-let TamanhoDaPeça = Math.floor(Math.random()*30);
-console.log(TamanhoDaPeça);
-let chupe = '';
+//let TamanhoDaPeça = Math.floor(Math.random()*30);
+//console.log(TamanhoDaPeça);
+//let chupe = '';
 
 
 
-while(chupe == ''){
-    chupe = prompt('Qual tamanho da minha peça? só o número de 0 a 30');
+//while(chupe == ''){
+  //  chupe = prompt('Qual tamanho da minha peça? só o número de 0 a 30');
 }
 
-if(TamanhoDaPeça == chupe){
-    alert('broxa kkkkkkkk');
-    console.log('broxa kkkkkkkk');
-} else{
-    alert('errou esquisito');
-    console.log('errou estranho');
+//if(TamanhoDaPeça == chupe){
+  //  alert('broxa kkkkkkkk');
+    //console.log('broxa kkkkkkkk');
+} //else{
+    //alert('errou esquisito');
+    //console.log('errou estranho');
 }
--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-alert('tobinha');
-let nomeUsuario = "";
-let anterior = document.querySelector('#texto');
+//alert('tobinha');
+//let nomeUsuario = "";
+//let anterior = document.querySelector('#texto');
 
-while(nomeUsuario == ""){
-    nomeUsuario = prompt('Qual tamanho das fezes?');
+//while(nomeUsuario == ""){
+  //  nomeUsuario = prompt('Qual tamanho das fezes?');
 }
 
-if(nomeUsuario == null){
-    anterior.textContent = 'anão';
-} else{
-    anterior.textContent = nomeUsuario;
+//if(nomeUsuario == null){
+  //  anterior.textContent = 'anão';
+} //else{
+    //anterior.textContent = nomeUsuario;
 }
 
 
